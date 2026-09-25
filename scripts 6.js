a94b7f1d6aa4ab90a1a5931d61b4edeff411e4fa
t1xqq <rasidozdemirov16@gmail.com>
2026-09-25 11:13:11 +0500

    шестая практическая

---
+++ scripts 6.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('.nav-menu');
+
+  navToggle.addEventListener('click', function () {
+    navMenu.classList.toggle('active');
+  });
+
+  // Закрытие меню при клике на ссылку
+  document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+      navMenu.classList.remove('active');
+    });
+  });
+});
